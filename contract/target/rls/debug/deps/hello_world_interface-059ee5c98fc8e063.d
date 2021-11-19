953c3e5548791219a45f4e0cd3acada51ea2551a
vadkutsen <vadkutsen@gmail.com>
2021-11-19 20:42:44 +0100

    123

---
+++ contract/target/rls/debug/deps/hello_world_interface-059ee5c98fc8e063.d
@@ -0,0 +1,5 @@
+/Users/vadkutsen/rust/near/hello-world-interface/contract/target/rls/debug/deps/hello_world_interface-059ee5c98fc8e063.rmeta: src/lib.rs
+
+/Users/vadkutsen/rust/near/hello-world-interface/contract/target/rls/debug/deps/hello_world_interface-059ee5c98fc8e063.d: src/lib.rs
+
+src/lib.rs:
---
+++ contract/target/rls/debug/deps/hello_world_interface-b2042e475e496228.d
@@ -0,0 +1,5 @@
+/Users/vadkutsen/rust/near/hello-world-interface/contract/target/rls/debug/deps/hello_world_interface-b2042e475e496228.rmeta: src/lib.rs
+
+/Users/vadkutsen/rust/near/hello-world-interface/contract/target/rls/debug/deps/hello_world_interface-b2042e475e496228.d: src/lib.rs
+
+src/lib.rs:
